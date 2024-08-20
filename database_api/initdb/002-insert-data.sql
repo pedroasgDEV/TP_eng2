23a37bcc21c9762e95002b77a78d1ca6cd47c836
Pedro Augusto Sousa Gonçalves <pedro.asg@aluno.ufop.edu.br>
2024-08-20 14:53:34 -0300

    create dockerfile & users, admin tables

---
+++ database_api/initdb/002-insert-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (regis_id, name, email, passwrd, inst, course)
+VALUES ('00.0.0000', 'the one', 'one@email.com', 'aboveall', 'begin', 'creation of all system');
+
+INSERT INTO admins (name, email, passwrd, derp)
+VALUES ('admin', 'admin@email.com', 'admin', 'ADM');
